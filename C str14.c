749478f6981a12604c0b8f3f118235372f796f40
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-11 15:37:06 +0530

    Create C str14.c

---
+++ C str14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i,words=1;
+    fgets(str,100,stdin);
+    for(i=0; str[i] != '\0'; i++) {
+        if(str[i] == ' ') {
+            words++;
+        }
+    }
+    printf("%d", words);
+    return 0;
+}
