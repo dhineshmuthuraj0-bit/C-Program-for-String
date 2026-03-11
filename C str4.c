55d407de5b32584a6ba34c1e491c4328bc777288
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-11 13:47:49 +0530

    Create C str4.c

---
+++ C str4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[20];
+    int i,len=0;
+    scanf("%s", str);
+    while(str[len]!='\0') {
+        len++;
+    }
+    for(i=len-1;i>=0;i--) {
+        printf("%c", str[i]);
+    }
+    return 0;
+}
